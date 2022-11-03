333e07899111146dbb3324b45f72169efdf697b7
thiquynh2004 <ntq.nguyenquynh@gmail.com>
2022-11-03 16:27:25 +0700

    back-to-top

--- page/Home/home.js
+++ page/Home/home.js
@@ -9,10 +9,38 @@ const showMenuToggle = () => {
 };
 menu.addEventListener("click", showMenuToggle);
 
-const setActive = (event) =>{
+const setActive = (event) => {
   console.log("e", event);
   document.querySelectorAll("#navMenu a.active").forEach(function (item) {
     item.classList.remove("active");
   });
   event.target.classList.add("active");
 };
+//back to top:
+let calcScroll = () => {
+  let scrollProgress = document.getElementById("progress");
+  let scrollProgressIcon = document.getElementById("progress-icon");
+  let pos = document.documentElement.scrollTop;
+  // console.log("pos", pos);
+
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  // console.log("calcHeight", calcHeight);
+
+  let scrollIcon = Math.round((pos * 100) / calcHeight);
+  // console.log("scrollIcon", scrollIcon);
+
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient( #fd4766 ${scrollIcon}%, #1d1d24 ${scrollIcon}%)`;
+};
+
+window.onscroll = calcScroll;
+window.onload = calcScroll;
